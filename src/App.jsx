3a512e9441051b9d4b6134e62c874c300925ffdc
EndiYmeri <56412395+EndiYmeri@users.noreply.github.com>
2022-01-20 01:05:25 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Route, Routes } from "react-router-dom";
+import LoggedInPage from "./pages/LoggedInPage";
+import LoginPage from "./pages/LoginPage";
+
+export default function App() {
+  return (
+    <div className="main-wrapper login">
+      <Routes>
+        <Route path="/login" element={<LoginPage/>}></Route>      
+        <Route path="/logged-in" element={<LoggedInPage/>}></Route>      
+      </Routes>
+
+
+    </div>
+  )
+  
+}
